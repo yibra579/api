cb6bcb7f0cf8c967ad4bf9348b8549d5314e1018
Yusuff Ibrahim <yusuffgrilloo@gmail.com>
2023-03-29 13:31:42 -0600

    server updated to receive model

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
+const mongoose = require('mongoose')
 const express = require('express')
+const Product = require('./models/apiModel')
+
 const app = express()
 
-// running port
-app.listen(3000, () => {
-    console.log('API running on port 3000')
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://grillo:159357@nodetuts.pgd5vzw.mongodb.net/node-API?retryWrites=true&w=majority').then(() => {
+    console.log('connected to database')
+    // running port
+    app.listen(3000, () => {
+        console.log('app running on port 3000');
+    })
+}).catch(err => {
+    console.log(err);
 })
 
 // routes
 app.get('/', (req, res) => {
     res.send('HELLO API')
-}) 
+})
+
+app.post('/product', async(req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product)
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).json({message: err.message})
+    }
+})
